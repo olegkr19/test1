594c60738d6bfb2b58dc3854e177d934c5c42abb
olegkr19 <45796213+olegkr19@users.noreply.github.com>
2020-03-24 11:33:39 +0200

    update db file

--- config/database.php
+++ config/database.php
@@ -50,7 +50,7 @@ return [
             'port' => env('DB_PORT','3306'),
             'database' => env('DB_DATABASE','bdcontro_oleg'),
             'username' => env('DB_USERNAME','bdcontro_oleg'),
-            'password' => env('DB_PASSWORD','Zo+;25Fr2d'),
+            'password' => env('DB_PASSWORD','***'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
